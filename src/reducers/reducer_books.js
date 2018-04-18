329e2a7c72b2e370153b1020b93bd5bfd215f1b8
Adam Kennedy <akennedy@nextnet.com>
2018-04-18 16:53:26 -0700

    adding untracked files

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'book 1', pages: 70},
+        {title: 'book 2', pages: 170},
+        {title: 'book 3', pages: 1170},
+        {title: 'book 4', pages: 370},
+    ]
+}
